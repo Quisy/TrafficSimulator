94d16e2ce7efe3e2d1b22df3e550ea74d0036e27
Quisy <lamprecht.mariusz@gmail.com>
2018-05-31 19:09:30 +0200

    Added empty .net core console app (#2)

---
+++ TrafficSimulator/TrafficSimulator.Cmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrafficSimulator.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
