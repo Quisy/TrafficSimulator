38841b8c020e8c0b54b0533308fef25f00f926a2
Quisy <lamprecht.mariusz@gmail.com>
2018-12-23 14:49:10 +0100

    Added move service

--- TrafficSimulator/TrafficSimulator.Models/Base/Vehicle.cs
+++ TrafficSimulator/TrafficSimulator.Models/Base/Vehicle.cs
@@ -1,16 +1,23 @@
 ﻿using System.Drawing;
+using System.Numerics;
 using TrafficSimulator.Enums;
 
 namespace TrafficSimulator.Models.Base
 {
     public abstract class Vehicle
     {
+        public Vector2 Position;
+
         public abstract VehicleType VehicleType { get; }
 
         public DriverType DriverType { get; set; }
 
-        public Point Position { get; set; }
-
+        
         public double CurrentSpeed { get; set; }
+
+        protected Vehicle(Vector2 startPosition)
+        {
+            this.Position = new Vector2(startPosition.X, startPosition.Y);
+        }
     }
 }
---
+++ TrafficSimulator/TrafficSimulator.Services/MoveService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrafficSimulator.Models.Vehicles;
+using TrafficSimulator.Services.Interfaces;
+
+namespace TrafficSimulator.Services
+{
+    public class MoveService : IMoveService
+    {
+        public void Drive(Car car, )
+        {
+            
+        }
+    }
+}
