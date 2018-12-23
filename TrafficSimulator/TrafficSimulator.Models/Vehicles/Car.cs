e9565b19a6c67be514a036a8a3e9e8329799bed2
Quisy <lamprecht.mariusz@gmail.com>
2018-12-23 14:49:10 +0100

    Added move service

--- TrafficSimulator/TrafficSimulator.Models/Vehicles/Car.cs
+++ TrafficSimulator/TrafficSimulator.Models/Vehicles/Car.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Numerics;
 using System.Text;
 using TrafficSimulator.Enums;
 using TrafficSimulator.Models.Base;
@@ -10,5 +11,18 @@ namespace TrafficSimulator.Models.Vehicles
     public class Car : Vehicle
     {
         public override VehicleType VehicleType => VehicleType.Car;
+
+        public Car(Vector2 startPosition) : base(startPosition)
+        {
+        }
+
+        public void Move(Vector2 position)
+        {
+            this.Position.X += position.X;
+            this.Position.Y += position.Y;
+        }
+
+
+       
     }
 }
