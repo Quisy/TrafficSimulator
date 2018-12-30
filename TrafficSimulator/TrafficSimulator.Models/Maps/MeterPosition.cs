fcf912db44287195497edd2e5708f5d5487eed27
Quisy <lamprecht.mariusz@gmail.com>
2018-12-30 22:52:43 +0100

    Added camera methods

---
+++ TrafficSimulator/TrafficSimulator.Models/Maps/MeterPosition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace TrafficSimulator.Models.Maps
+{
+    public class MeterPosition
+    {
+        public int X { get; set; };
+
+        public int Y { get; set; };
+    }
+}
