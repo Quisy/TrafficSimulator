71e595ed22a0df4473a891b31c9a251a59dd9897
Quisy <lamprecht.mariusz@gmail.com>
2019-01-06 12:59:26 +0100

    Created camera model, moved camera methods from service to new model

---
+++ TrafficSimulator/TrafficSimulator.Enums/CameraType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrafficSimulator.Enums
+{
+    public enum CameraType
+    {
+        Front,
+        Back,
+        Left,
+        Right
+    }
+}
