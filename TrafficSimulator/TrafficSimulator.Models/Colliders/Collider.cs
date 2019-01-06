095706c330b3aab653ea84481e9734ef42e7639c
Quisy <lamprecht.mariusz@gmail.com>
2019-01-06 16:00:11 +0100

    Addec box collider

---
+++ TrafficSimulator/TrafficSimulator.Models/Colliders/Collider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace TrafficSimulator.Models.Colliders
+{
+    public abstract class Collider
+    {
+        public List<Vector2> Points;
+        public abstract void ChangePosition(Vector2 position);
+        public abstract void ChangeSize(Vector2 size);
+        public abstract bool CheckCollision(Collider collider);
+    }
+}
