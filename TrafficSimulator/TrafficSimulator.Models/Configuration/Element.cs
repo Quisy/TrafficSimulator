e4fe0de54e19d7eae63abfa161425678b8e68bcc
Quisy <lamprecht.mariusz@gmail.com>
2019-01-27 18:10:10 +0100

    Added configuration classes

---
+++ TrafficSimulator/TrafficSimulator.Models/Configuration/Element.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrafficSimulator.Models.Configuration
+{
+    public class Element
+    {
+        public MapPoint Position { get; set; }
+        public int Size { get; set; }
+        public ElementType ElementType { get; set; }
+    }
+}
---
+++ TrafficSimulator/TrafficSimulator.Models/Configuration/Track.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrafficSimulator.Models.Configuration
+{
+    public class Track
+    {
+        public string Name { get; set; }
+        public string CarName { get; set; }
+        public MapPoint StartPoint { get; set; }
+        public MapPoint EndPoint { get; set; }
+        public MapPoint[] ControlPoints { get; set; }
+    }
+}
